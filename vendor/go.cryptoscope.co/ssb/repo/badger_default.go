578d81f0fbd910b5b7f7816b22c26b334b23feda
Piet Geursen <pietgeursen@gmail.com>
2019-12-19 12:12:29 +0100

    fixes for arm, adds ip and stuff to status message

--- vendor/go.cryptoscope.co/ssb/repo/badger_default.go
+++ vendor/go.cryptoscope.co/ssb/repo/badger_default.go
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: MIT
 
-// +build freebsd linux windows darwin,amd64 darwin,386
-
 package repo
 
 import (
 	"github.com/dgraph-io/badger"
+	"github.com/dgraph-io/badger/options"
 )
 
 func badgerOpts(dbPath string) badger.Options {
-	return badger.DefaultOptions(dbPath)
+	opts := badger.DefaultOptions(dbPath)
+
+	// runtime throws MMIO can't allocate errors without this
+	// => badger failed to open: Invalid ValueLogLoadingMode, must be FileIO or MemoryMap
+	opts.ValueLogLoadingMode = options.FileIO
+	return opts
 }
